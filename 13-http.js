2a847c919bd24becaa234a25682dad7263da22df
Kabin <kabinghale1@gmail.com>
2024-05-24 13:31:35 +0545

    http js module

---
+++ 13-http.js
@@ -0,0 +1,35 @@
+const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.end("This is home page");
+//   }
+
+//   if (req.url === "/about") {
+//     res.end("This is about page");
+//   }
+
+//   res.end(`
+//      <h1>Oops!</h1>
+//      <p>We can't seem to find the page you are looking for</p>
+//      <a href="/">back home</a>
+//    `);
+// });
+
+//Above method is return error for some reason
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.end("Welcome to our home page");
+  } else if (req.url === "/about") {
+    res.end("Here is our about page");
+  } else {
+    res.end(`
+    <h1>Oops!</h1>
+    <p>We can't seem to find the page you are looking for</p>
+    <a href="/">back home</a>
+    `);
+  }
+});
+
+server.listen(5000);
